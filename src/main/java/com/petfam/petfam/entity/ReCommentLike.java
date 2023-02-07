7f1353ba91c6fade5e043e705cee19cab249b122
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-02-07 11:57:26 +0900

    #5[Feat] Recommentlike Entity 생성

---
+++ src/main/java/com/petfam/petfam/entity/ReCommentLike.java
@@ -0,0 +1,51 @@
+package com.petfam.petfam.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ReCommentLike {
+
+  /**
+   * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+
+  /**
+   * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
+   */
+  @Builder
+  public ReCommentLike(User user, ReComment reComment) {
+    this.user = user;
+    this.reComment = reComment;
+  }
+
+  /**
+   * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
+   */
+  @ManyToOne
+  private User user;
+
+  @ManyToOne
+  private ReComment reComment;
+
+  /**
+   * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
+   */
+
+  /**
+   * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
+   */
+
+}
