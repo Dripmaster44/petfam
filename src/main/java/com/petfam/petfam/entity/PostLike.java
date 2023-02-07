231dca6460955071a46077331f1ecf7ae3e32bbe
DRIPMASTER <qsccsq4444@naver.com>
2023-02-07 16:18:45 +0900

    #7 [refactor] GenerationType.Auto -> GenerationType.IDENTITY 로 변경

--- src/main/java/com/petfam/petfam/entity/PostLike.java
+++ src/main/java/com/petfam/petfam/entity/PostLike.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class PostLike {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JoinColumn
