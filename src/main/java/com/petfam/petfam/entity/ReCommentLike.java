3b4d44d4fe84d1b3de94d1f39c5a769943953c4a
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-02-07 12:45:22 +0900

    #5[refactoring] ReCommentLike entity코드리펙터링

--- src/main/java/com/petfam/petfam/entity/ReCommentLike.java
+++ src/main/java/com/petfam/petfam/entity/ReCommentLike.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,38 +15,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReCommentLike {
 
-  /**
-   * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
-   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
 
-  /**
-   * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
-   */
   @Builder
   public ReCommentLike(User user, ReComment reComment) {
     this.user = user;
     this.reComment = reComment;
   }
 
-  /**
-   * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
-   */
+
   @ManyToOne
+  @JoinColumn
   private User user;
 
   @ManyToOne
+  @JoinColumn
   private ReComment reComment;
 
-  /**
-   * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
-   */
 
-  /**
-   * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
-   */
 
 }
