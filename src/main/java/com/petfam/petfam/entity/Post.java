f20d965cbf7cd9c67cc65493db0312892b465778
haeri <hadi43@naver.com>
2023-02-08 09:10:54 +0900

    #17 [feat] Post에 좋아요 관련 부분 수정 -> list에서 integer로 변경

--- src/main/java/com/petfam/petfam/entity/Post.java
+++ src/main/java/com/petfam/petfam/entity/Post.java
@@ -1,51 +1,62 @@
 package com.petfam.petfam.entity;
 
 import com.petfam.petfam.entity.enums.CategoryEnum;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor
 public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @JoinColumn
-    @ManyToOne
-    private User user;
+  @JoinColumn
+  @ManyToOne
+  private User user;
 
-    @Column(nullable = false)
-    private String title;
+  @Column(nullable = false)
+  private String title;
 
-    @Column(nullable = false)
-    private String content;
+  @Column(nullable = false)
+  private String content;
 
-    @Column
-    private String image;
+  @Column
+  private String image;
 
-    @JoinColumn
-    @OneToMany
-    private List<PostLike> postLikes = new ArrayList<>();
+  private Integer postLikes = 0;
 
-    @JoinColumn
-    @OneToMany
-    private List<Comment> comments = new ArrayList<>();
+  @JoinColumn
+  @OneToMany
+  private List<Comment> comments = new ArrayList<>();
 
 
-    private CategoryEnum category;
+  private CategoryEnum category;
 
-    @Builder
-    public Post(String title, String content, String image) {
-        this.title = title;
-        this.content = content;
-        this.image = image;
+  @Builder
+  public Post(String title, String content, String image) {
+    this.title = title;
+    this.content = content;
+    this.image = image;
+  }
+
+  public void updateLike(boolean islike) {
+    postLikes += islike ? 1 : -1;
+    if (postLikes < 0) {
+      postLikes = 0;
     }
+  }
 }
