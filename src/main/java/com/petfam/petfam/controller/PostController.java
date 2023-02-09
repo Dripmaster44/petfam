bf71a15ee9c4f37c36455c5c5c165c585c4dfd9c
haeri <hadi43@naver.com>
2023-02-09 10:35:20 +0900

    #19 [refactor] PostController와 PostService연관관계 설정 및 RequestMapping 애노테이션 사용

--- src/main/java/com/petfam/petfam/controller/PostController.java
+++ src/main/java/com/petfam/petfam/controller/PostController.java
@@ -1,7 +1,12 @@
 package com.petfam.petfam.controller;
 
+import com.petfam.petfam.dto.post.AllPostResponseDto;
 import com.petfam.petfam.dto.post.PostCreateRequestDto;
+import com.petfam.petfam.dto.post.PostResponseDto;
+import com.petfam.petfam.dto.post.PostUpdateRequestDto;
+import com.petfam.petfam.dto.post.PostUpdateResponseDto;
 import com.petfam.petfam.security.UserDetailsImpl;
+import com.petfam.petfam.service.post.PostServiceImpl;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,42 +16,44 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
 
   private final PostServiceImpl postService;
 
   // 게시글 작성
-  @PostMapping("/posts")
+  @PostMapping("")
   public void createPost(@RequestBody PostCreateRequestDto postCreateRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     postService.createPost(postCreateRequestDto);
   }
 
   // 게시글 전체 목록 조회
-  @GetMapping("/posts")
+  @GetMapping("")
   public List<AllPostResponseDto> getAllPosts() {
     return postService.getAllPosts();
   }
 
   // 선택 게시글 조회
-  @GetMapping("/posts/{id}")
+  @GetMapping("/{id}")
   public PostResponseDto getSelectPost(@PathVariable Long id) {
     return postService.getSelectPost(id);
   }
 
-  @PatchMapping("/posts/{id}")
+  @PatchMapping("/{id}")
   public PostUpdateResponseDto updatePost(@PathVariable Long id,
       @RequestBody PostUpdateRequestDto postUpdateRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     return postService.updatePost(id, postUpdateRequestDto);
   }
 
-  @DeleteMapping("/posts/{id}")
+  @DeleteMapping("/{id}")
   public void deletePost(@PathVariable Long id,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
     postService.deletePost(id);
