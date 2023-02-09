127b7595ff585cca80306f4515469b00cff32c80
EunchongJo <agapejo456@naver.com>
2023-02-09 11:54:46 +0900

    #24[refactor] 충돌로 인한 재 PR

--- src/main/java/com/petfam/petfam/controller/AdminController.java
+++ src/main/java/com/petfam/petfam/controller/AdminController.java
@@ -3,13 +3,9 @@ package com.petfam.petfam.controller;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;import com.petfam.petfam.dto.user.UserResponseDto;
-import com.petfam.petfam.security.UserDetailsImpl;
 import com.petfam.petfam.service.user.AdminServiceImpl;
 import lombok.RequiredArgsConstructor;
 
