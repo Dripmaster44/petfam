5b01f90f9ca7defa4c59a9b8863c3226f80b0fc7
haeri <hadi43@naver.com>
2023-02-19 14:53:16 +0900

    #50 [!BREAKING CHANGE] RESTFUL한 api설계를 위해 댓글 생성, 대댓글 생성 메소드 class 이동

--- src/main/java/com/petfam/petfam/controller/ReCommentController.java
+++ src/main/java/com/petfam/petfam/controller/ReCommentController.java
@@ -2,7 +2,6 @@ package com.petfam.petfam.controller;
 
 import com.petfam.petfam.dto.recomment.ReCommentRequestDto;
 import com.petfam.petfam.security.UserDetailsImpl;
-import com.petfam.petfam.service.recomment.ReCommentService;
 import com.petfam.petfam.service.recomment.ReCommentServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,26 +21,22 @@ public class ReCommentController {
 
   private final ReCommentServiceImpl reCommentService;
 
-  // 대댓글 생성
-  @PostMapping("{commentId}")
-  public ResponseEntity<String> reComment(@PathVariable Long commentId, @RequestBody ReCommentRequestDto reCommentRequestDto,
-      @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.OK).body( reCommentService.reComment(commentId, userDetails.getUser(), reCommentRequestDto));
-
-  }
 
   // 대댓글 수정
   @PatchMapping("/{recommentId}")
-  public ResponseEntity<String> updateReComment(@PathVariable Long recommentId, @RequestBody ReCommentRequestDto reCommentRequestDto,
+  public ResponseEntity<String> updateReComment(@PathVariable Long recommentId,
+      @RequestBody ReCommentRequestDto reCommentRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.OK).body(reCommentService.updateReComment(recommentId, userDetails.getUser(), reCommentRequestDto));
+    return ResponseEntity.status(HttpStatus.OK).body(
+        reCommentService.updateReComment(recommentId, userDetails.getUser(), reCommentRequestDto));
   }
 
   // 대댓글 삭제
   @DeleteMapping("/{recommentId}")
   public ResponseEntity<String> deleteReComment(@PathVariable Long recommentId,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.OK).body(reCommentService.deleteReComment(recommentId, userDetails.getUser()));
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(reCommentService.deleteReComment(recommentId, userDetails.getUser()));
 
   }
 
