938c9aa7791f9654ab6682cd4c010e73ad8259f4
haeri <hadi43@naver.com>
2023-02-28 20:06:27 +0900

    #50 [comment] 필요한 주석 추가

--- src/main/java/com/petfam/petfam/entity/TimeStamped.java
+++ src/main/java/com/petfam/petfam/entity/TimeStamped.java
@@ -9,8 +9,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass  // 상속시, 자동으로 컬럼으로 인식하게 함
+@EntityListeners(AuditingEntityListener.class)  // 시간을 자동으로 반영하도록 설정해줌
 public class TimeStamped {
 
   @CreatedDate
